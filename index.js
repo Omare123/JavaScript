1adb035217e0162a34b69f5b6cb2abb4a6157cb1
Omare123 <ojrojasr@hotmail.com>
2021-12-18 10:04:11 -0300

    condicionales

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 let primeraVariable = prompt("Inserte un número", 0)
 let segundaVariable = prompt("Inserte otro número", 0)
+showMessage(primeraVariable, segundaVariable)
 
-alert(`Los números multiplicados son ${parseInt(primeraVariable)*parseInt(segundaVariable)}`)
+function showMessage(primeraVariable, segundaVariable){
+    let parsedPrimera = parseFloat(primeraVariable);
+    let parsedSegunda = parseFloat(segundaVariable);
+    if(!isNaN(parsedPrimera) || !isNaN(parsedSegunda))
+        alert(`Los números multiplicados son ${parsedPrimera*parsedSegunda}`)
+    else
+        alert(`Los datos ingresados no son válidos`)
+}
