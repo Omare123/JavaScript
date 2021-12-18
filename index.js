07d74e2148e531e66165d338cae422cea7159896
Omare123 <ojrojasr@hotmail.com>
2021-12-18 09:55:33 -0300

    firts commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let primeraVariable = prompt("Inserte un número", 0)
+let segundaVariable = prompt("Inserte otro número", 0)
+
+alert(`Los números multiplicados son ${parseInt(primeraVariable)*parseInt(segundaVariable)}`)
