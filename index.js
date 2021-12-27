ed04e42b34217f78440f7c448ff3ffeee6359fb1
Omare123 <ojrojasr@hotmail.com>
2021-12-27 10:34:18 -0300

    simulador de banco

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-let primeraVariable = prompt("Inserte un número", 0)
-let segundaVariable = prompt("Inserte otro número", 0)
-showMessage(primeraVariable, segundaVariable)
+let depositButton = document.getElementById("deposit")
+let withdrawButton = document.getElementById("withdraw")
+let accoutMoney = document.getElementById("money")
+begin();
 
-function showMessage(primeraVariable, segundaVariable){
-    let parsedPrimera = parseFloat(primeraVariable);
-    let parsedSegunda = parseFloat(segundaVariable);
-    if(!isNaN(parsedPrimera) || !isNaN(parsedSegunda))
-        alert(`Los números multiplicados son ${parsedPrimera*parsedSegunda}`)
-    else
-        alert(`Los datos ingresados no son válidos`)
+const isValid = (number) => !isNaN(number) && number >= 0
+function begin() {
+    accoutMoney.innerText = 1000
+    withdrawButton.addEventListener("click", withdraw)
+    depositButton.addEventListener("click", deposit)
 }
+
+function withdraw() {
+    let withdraw = parseFloat(prompt("¿Cuánto dinero desea extraer?", 0))
+    if(isValid(withdraw)){
+        if(withdraw <= accoutMoney.innerText)
+            accoutMoney.innerText -= withdraw
+        else
+            alert("No hay suficiente dinero en tu cuenta")
+    }
+    else
+        alert("Numero invalido")
+    
+}
+
+function deposit() {
+    let deposit = parseFloat(prompt("¿Cuánto dinero desea depositar?", 0))
+    if(isValid(deposit))
+        accoutMoney.innerText = parseFloat(accoutMoney.innerText) + deposit
+    else
+        alert("Número invalido")
+}
+
