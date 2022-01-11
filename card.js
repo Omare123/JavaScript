bde8dc4196ff05f577895cb6d9d48d11aa25ac4b
Omar <ojrojasr@hotmail.com>
2022-01-11 16:40:36 -0300

    Objetos

---
+++ card.js
@@ -0,0 +1,24 @@
+export default class Card{
+    constructor(name){
+        this.name = name
+        this.number = "4832 6687 3457 " + Math.floor(Math.random() * (9999 - 999 + 1) + 999).toString()
+        this.emitionDate = new Date();
+        this.expirationDate = this.getExpirationDate(this.emitionDate)
+    }
+    getExpirationDate (emitionDate) {
+        let response = new Date(emitionDate)
+        response.setFullYear(response.getFullYear() + 6)
+        return response
+    }
+    executePayment (accoutMoney){
+        let amount = parseFloat(prompt("¿Cuánto dinero desea pagar?", 0))
+        if(!isNaN(amount) && amount >= 0){
+            if(amount <= accoutMoney.innerText)
+                accoutMoney.innerText -= amount
+            else
+                alert("No hay suficiente dinero en tu cuenta")
+        }
+        else
+            alert("Numero invalido")
+    }
+}
--- index.js
+++ index.js
@@ -1,61 +1,54 @@
+import Card from "./card.js"
 let depositButton = document.getElementById("deposit")
 let withdrawButton = document.getElementById("withdraw")
 let accoutMoney = document.getElementById("money")
-let card = document.getElementById("card")
-const username = `${prompt("¿Cuál es su nombre?")} ${prompt("¿Cuál es su apellido?")}`
-let personalCard ;
+let cardButton = document.getElementById("cardButton").addEventListener("click", setCard)
+let username = ""
+let cards = []
 
-
-class Card {
-    constructor(name){
-        this.name = name
-        this.number = "4832 6687 3457 " + Math.floor(Math.random() * (9999 - 999 + 1) + 999).toString()
-        this.emitionDate = new Date();
-        this.expirationDate = this.getExpirationDate(this.emitionDate)
-    }
-    getExpirationDate (emitionDate) {
-        let response = new Date(emitionDate)
-        response.setFullYear(response.getFullYear() + 6)
-        return response
-    }
-    executePayment (){
-        let amount = parseFloat(prompt("¿Cuánto dinero desea pagar?", 0))
-        if(!isNaN(amount) && amount >= 0){
-            if(amount <= accoutMoney.innerText)
-                accoutMoney.innerText -= amount
-            else
-                alert("No hay suficiente dinero en tu cuenta")
-        }
-        else
-            alert("Numero invalido")
-    }
-}
+const isValidNumber = (number) => !isNaN(number) && number >= 0
+const cardsLimit = () => cards.length < 3
+const toCapitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 
 begin();
-
-
-const isValid = (number) => !isNaN(number) && number >= 0
 function begin() {
     accoutMoney.innerText = 1000
     withdrawButton.addEventListener("click", withdraw)
     depositButton.addEventListener("click", deposit)
-    this.personalCard = new Card(username)
-    this.setCard()
+    username = askName()
+}
+function askName(){
+    let name = prompt("¿Cuál es su nombre?")
+    let lastName = prompt("¿Cuál es su apellido?")
+    name && lastName ? null : document.location.reload()
+    return `${toCapitalize(name)} ${toCapitalize(lastName)}`
+}
+function printCard(){
+    let personalCard = new Card(username)
+    cards.push(personalCard)
+    let cardsDiv = document.getElementById("cards")
+    let cardExpiration = document.createElement("p").innerText = `desde ${personalCard.emitionDate.getMonth() + 1}/${personalCard.emitionDate.getFullYear()}    hasta ${personalCard.expirationDate.getMonth() + 1}/${personalCard.expirationDate.getFullYear()}`
+    let tag = document.createElement("div")
+    tag.innerHTML =
+     `<p class="h3" id="cardNumber">${personalCard.number}</p>
+    <p>${cardExpiration}</p>
+    <p>${personalCard.name}</p>
+    <button type="button" class="btn btn-dark w-100" id="${personalCard.number}">pagar</button>`;
+    tag.setAttribute("class","w-25 bg-warning p-2 mt-3 rounded");
+    cardsDiv.appendChild(tag);
+    var payButton = document.getElementById(personalCard.number.toString());
+    payButton.addEventListener("click", () => personalCard.executePayment(accoutMoney))
 }
 function setCard(){
-    let name = document.getElementById("cardName");
-    let number = document.getElementById("cardNumber");
-    let expiration = document.getElementById("cardExp");
-    let payButton = document.getElementById("pay"); 
-    name.innerText = this.personalCard.name;
-    number.innerText = this.personalCard.number;
-    expiration.innerText = `desde ${this.personalCard.emitionDate.getMonth() + 1}/${this.personalCard.emitionDate.getFullYear()}    hasta ${this.personalCard.expirationDate.getMonth() + 1}/${this.personalCard.expirationDate.getFullYear()}`
-    payButton.addEventListener("click", this.personalCard.executePayment)
+    if(cardsLimit())
+        printCard()
+    else
+        alert("Llegaste a tu límite de tarjetas")
 }
 
 function withdraw() {
     let withdraw = parseFloat(prompt("¿Cuánto dinero desea extraer?", 0))
-    if(isValid(withdraw)){
+    if(isValidNumber(withdraw)){
         if(withdraw <= accoutMoney.innerText)
             accoutMoney.innerText -= withdraw
         else
@@ -68,7 +61,7 @@ function withdraw() {
 
 function deposit() {
     let deposit = parseFloat(prompt("¿Cuánto dinero desea depositar?", 0))
-    if(isValid(deposit))
+    if(isValidNumber(deposit))
         accoutMoney.innerText = parseFloat(accoutMoney.innerText) + deposit
     else
         alert("Número invalido")
