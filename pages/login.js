86c37d969e702a16898b119662ac24613da81848
Omar <ojrojasr@hotmail.com>
2022-02-07 21:26:57 -0300

    AJAX

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,27 @@
 import toCapitalize from "../helpers/capitalize.js"
-let firstName = document.getElementById("nombre");
-let lastName = document.getElementById("apellido");
+let mail = document.getElementById("mail");
+let password = document.getElementById("password");
 import showError from '../helpers/showErrors.js'
 document.getElementById("login").addEventListener("click", login)
+document.getElementById("help").addEventListener("click", showHelp)
+let url = 'https://www.mecallapi.com/api/login'
+$('#helpInfo').hide()
+
 
 function login(){
-    if(firstName.value && lastName.value){
-        localStorage.setItem('firstName', toCapitalize(firstName.value));
-        localStorage.setItem('lastName', toCapitalize(lastName.value));
-        window.location.replace("../index.html")
+    if(mail.value && password.value){
+        let data = {
+            "username": mail.value,
+            "password": password.value
+        }
+        getData(url, data).then(response => {
+            localStorage.setItem('firstName', toCapitalize(response.user.fname));
+            localStorage.setItem('lastName', toCapitalize(response.user.lname));   
+            window.location.replace("../index.html")         
+        }).catch(err => {
+           showError("Alguno de los datos es invalido")
+        })
+        
     }
     else{
         if(!firstName.value){
@@ -19,3 +32,19 @@ function login(){
     }
         
 }
+
+function showHelp(){
+    $('#helpInfo').text("username = karn.yong@mecallapi.com password = mecallapi")
+    $('#helpInfo').toggle()
+}
+
+function getData(ajaxurl, data) { 
+    return $.ajax({
+      url: ajaxurl,
+      type: 'POST',
+      data: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  };
